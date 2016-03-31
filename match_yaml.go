5e3c218c80b980428e21871d0bb593b0df74a0b4
George Dean <gdean@pivotal.io>
2016-03-31 10:56:46 -0700

    Improve variable naming
    [#116462197]

--- match_yaml.go
+++ match_yaml.go
@@ -28,14 +28,14 @@ func (matcher *MatchYAMLMatcher) Match(actual interface{}) (success bool, err er
 		return false, err
 	}
 
-	var aval interface{}
-	var eval interface{}
+	var actualValue interface{}
+	var expectedValue interface{}
 
 	// this is guarded by prettyPrint
-	candiedyaml.Unmarshal([]byte(actualString), &aval)
-	candiedyaml.Unmarshal([]byte(expectedString), &eval)
+	candiedyaml.Unmarshal([]byte(actualString), &actualValue)
+	candiedyaml.Unmarshal([]byte(expectedString), &expectedValue)
 
-	return reflect.DeepEqual(aval, eval), nil
+	return reflect.DeepEqual(actualValue, expectedValue), nil
 }
 
 func (matcher *MatchYAMLMatcher) FailureMessage(actual interface{}) (message string) {
@@ -65,20 +65,20 @@ func (matcher *MatchYAMLMatcher) prettyPrint(input interface{}) (formatted strin
 	return string(buf), nil
 }
 
-func toString(a interface{}) (string, bool) {
-	aString, isString := a.(string)
+func toString(value interface{}) (string, bool) {
+	valueString, isString := value.(string)
 	if isString {
-		return aString, true
+		return valueString, true
 	}
 
-	aBytes, isBytes := a.([]byte)
+	valueBytes, isBytes := value.([]byte)
 	if isBytes {
-		return string(aBytes), true
+		return string(valueBytes), true
 	}
 
-	aStringer, isStringer := a.(fmt.Stringer)
+	valueStringer, isStringer := value.(fmt.Stringer)
 	if isStringer {
-		return aStringer.String(), true
+		return valueStringer.String(), true
 	}
 
 	return "", false
